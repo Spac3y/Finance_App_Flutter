c3fdc64a2966646a36a3bb35f6ffbad8c6f2b6b6
Spac3y <73653851+Spac3y@users.noreply.github.com>
2022-05-06 22:29:43 +0300

    Update outdated packages

--- finacash/lib/main.dart
+++ finacash/lib/main.dart
@@ -1,18 +1,12 @@
-
 import 'package:finacash/screen/InicialPage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 main() {
-  
-
-  initializeDateFormatting().then((_){
+  initializeDateFormatting().then((_) {
     runApp(MaterialApp(
-    home: InicialPage(),
-    debugShowCheckedModeBanner: false,
-    
-  ));
+      home: InicialPage(),
+      debugShowCheckedModeBanner: false,
+    ));
   });
-  
 }

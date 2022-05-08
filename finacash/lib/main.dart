132cb87b3eb82552e8f4a93a840af980ac325ca3
Spac3y <73653851+Spac3y@users.noreply.github.com>
2022-05-08 18:16:09 +0300

    Translate main page

--- finacash/lib/main.dart
+++ finacash/lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:finacash/screen/InicialPage.dart';
+import 'package:finacash/screen/InitialPage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 main() {
   initializeDateFormatting().then((_) {
     runApp(MaterialApp(
-      home: InicialPage(),
+      home: InitialPage(),
       debugShowCheckedModeBanner: false,
     ));
   });

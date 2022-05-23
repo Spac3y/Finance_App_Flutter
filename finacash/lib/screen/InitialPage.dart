0c561e3de0a3501450ccf53168c6ab2d6cddf633
Spac3y <73653851+Spac3y@users.noreply.github.com>
2022-05-23 19:35:08 +0300

    wordlist on initial page

--- finacash/lib/screen/InitialPage.dart
+++ finacash/lib/screen/InitialPage.dart
@@ -4,30 +4,34 @@ import 'package:finacash/screen/HomePage.dart';
 import 'package:finacash/screen/ReveneuSummary.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import "package:finacash/global.dart" as globals;
+
+final List<String> wordList = ['Expenses', 'Cheltuieli'];
+final List<String> word1List = ['Home', 'Acasa'];
+final List<String> word2List = ['Reveneu', 'Venituri'];
 
 class InitialPage extends StatefulWidget {
   final List<BarItem> barItems = [
     BarItem(
-      text: "Expenses",
+      text: wordList[globals.languageNumber],
       iconData: Icons.remove_circle_outline,
       color: Colors.pinkAccent,
     ),
     BarItem(
-      text: "Home",
+      text: word1List[globals.languageNumber],
       iconData: Icons.home,
       color: Colors.indigo,
     ),
     BarItem(
-      text: "Revenue",
+      text: word2List[globals.languageNumber],
       iconData: Icons.add_circle_outline,
       color: Colors.teal,
     ),
-    /*BarItem(
+    BarItem(
       text: "Search",
       iconData: Icons.search,
-      color: Colors.yellow.shade900,
+      // color: Colors.yellow.shade900,
     ),
-    */
   ];
 
   @override
@@ -40,7 +44,6 @@ class _InitialPageState extends State<InitialPage> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
 
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
         //systemNavigationBarColor: Colors.lightBlue[700], // navigation bar color

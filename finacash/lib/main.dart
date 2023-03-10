2613856efec3dfa6000c85dfc19526d1358ac2d5
Spac3y <73653851+Spac3y@users.noreply.github.com>
2023-03-11 01:06:28 +0200

    idk wtf am doin

--- finacash/lib/main.dart
+++ finacash/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:MondayReport/screen/InitialPage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() {
   initializeDateFormatting().then((_) {
